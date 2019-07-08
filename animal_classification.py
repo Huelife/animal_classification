5b77ae05d1b40b4352fd6717bec0aa146b21a073
Eric Meas <emmeas@gmail.com>
2019-07-07 22:36:55 -0700

    Update animal_classification.py
    adding classes and class names

--- animal_classification.py
+++ animal_classification.py
@@ -1,1 +1,33 @@
 #animal_classification.py: Separating animals into different groups
+
+class Kingdom():
+  def __init__(self,name):
+    self.name = "Animalia"
+
+class Phylum(Kingdom):
+  Chordata
+  
+class Class(Phylum):
+  Mammalia
+  
+class Order(Class):
+  Carnivora
+
+class Suborder(Order):
+  Feliformia
+  
+class Family(Suborder):
+  Felidae
+  
+class Subfamily(Family):
+  Felinae
+  
+class Genus(Subfamily):
+  Felis
+  
+class Species(Genus):
+  F. catus
+  Felis catus
+  
+class CommonName(Species):
+  Cat
